771b0f36ec6f6cf261f6b35fa47397bf10998566
坂本葉 <YoSakamoto@sakamotoyou-no-MacBook-Pro.local>
2015-06-23 19:18:03 +0900

    first commit

---
+++ Drawing/StampViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StampViewController.swift
+//  Drawing
+//
+//  Created by 坂本葉 on 2015/06/21.
+//  Copyright (c) 2015年 YoSakamoto. All rights reserved.
+//
+
+import Cocoa
+
+class StampViewController: UIViewController {
+
+}
