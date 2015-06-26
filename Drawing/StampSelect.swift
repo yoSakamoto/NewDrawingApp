3647ff84eef37d2323af1ce30346f0495e1422d7
坂本葉 <YoSakamoto@sakamotoyou-no-MacBook-Pro.local>
2015-06-26 13:40:02 +0900

    second commit

---
+++ Drawing/StampSelect.swift
@@ -0,0 +1,13 @@
+//
+//  StampSelect.swift
+//  Drawing
+//
+//  Created by 坂本葉 on 2015/06/25.
+//  Copyright (c) 2015年 YoSakamoto. All rights reserved.
+//
+
+import Cocoa
+
+class StampSelect: UIImageView {
+
+}
