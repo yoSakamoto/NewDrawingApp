e3209658bf7f6957137af331309eb3f28b45159a
坂本葉 <YoSakamoto@sakamotoyou-no-MacBook-Pro.local>
2015-06-27 10:26:45 +0900

    いけてる?

--- Drawing/StampSelect.swift
+++ Drawing/StampSelect.swift
@@ -6,8 +6,23 @@
 //  Copyright (c) 2015年 YoSakamoto. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class StampSelect: UIImageView {
-
+    
+    //描画
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.superview?.bringSubviewToFront(self)
+        
+    }
+    
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        let x = touch.locationInView(self.superview).x - touch.previousLocationInView(self.superview).x
+        let y = touch.locationInView(self.superview).y - touch.previousLocationInView(self.superview).y
+        self.center = CGPointMake(self.center.x + x, self.center.y + y)
+    }
+    
+    
 }
--- Drawing/StampViewController.swift
+++ Drawing/StampViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class StampViewController: UIViewController {
     
-
+    //imageArray[UIImage]
+    var imageArray:[UIImage] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for i in 1...10 {
+            imageArray.append(UIImage(named: "\(i).png")!)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //スタンプ表示(CollectionView)
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var Cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! UICollectionViewCell
+        let imageView = Cell.viewWithTag(1) as! UIImageView
+        imageView.image = imageArray[indexPath.row]
+        return Cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let stamp = StampSelect()
+        stamp.image = imageArray[indexPath.row]
+        var appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.StampArray.append(stamp)
+        appDelegate.isNewStamAdded = true
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
     
     @IBAction func backTapped(){
         self.dismissViewControllerAnimated(true, completion: nil)
-
+        
     }
+    
+    
 }
